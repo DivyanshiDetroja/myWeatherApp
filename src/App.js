2dea8319b4b807f371884d0f5d8450f916ca80a9
DivyanshiDetroja <107313235+DivyanshiDetroja@users.noreply.github.com>
2023-11-07 00:50:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   useEffect(() => {
 
     const getReq = async () => {
-      await axios.get("https://weather-42t16aeub-fxlgun.vercel.app/weather").then((response) => {
+      await axios.get("https://weather-5keym30nx-divyanshidetroja.vercel.app/weather").then((response) => {
       setData(response.data);
     })
       
